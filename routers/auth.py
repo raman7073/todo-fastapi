7bc88a7a44eaf8498a1bf7d2357533c89c986735
Raman Mehta <raman.mehta@fiftyfivetech.io>
2023-11-01 13:10:12 +0530

    Adding authentication

--- routers/auth.py
+++ routers/auth.py
@@ -1,19 +1,61 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from models import Users
+from passlib.context import CryptContext
+from database import SessionLocal
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+ 
+def authenticate_user(username:str,password:str,db):
+
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password,user.hashed_password):
+        return False
+    return True
 
 class UserRequest(BaseModel):
     email: str
     username: str
     first_name: str
     last_name: str
-    hashed_password: str
+    password: str
     role: str
 
 
-@router.get("/auth/")
-async def get_user(user_request: UserRequest):
-    user_model = Users(**user_request.model_dump())
+@router.post("/auth")
+async def create_user(db: db_dependency, user_request: UserRequest):
+
+    user_model = Users(
+        email=user_request.email,
+        username=user_request.username,
+        first_name=user_request.first_name,
+        last_name=user_request.last_name,
+        role=user_request.role,
+        hashed_password=bcrypt_context.hash(user_request.password),
+        is_active=True
+    )
+    db.add(user_model)
+    db.commit()
+
+@router.post("/token")
+async def login_for_access_token(form_data:Annotated[OAuth2PasswordRequestForm,Depends()],db:db_dependency):
+    
+    user =  authenticate_user(form_data.username ,  form_data.password ,db)
